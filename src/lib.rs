e4b20ece4c1a6b58be2b6dc0a0a519aae3273085
elsuizo <mnoblia@disroot.org>
2019-04-10 13:45:02 -0300

    pruebo en este branch con el crate nalgebra

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 /// A Robotics library in Rust programming language
 ///
 
-extern crate ndarray;
-extern crate ndarray_linalg;
+// extern crate ndarray;
+// extern crate ndarray_linalg;
+extern crate nalgebra as na;
 extern crate assert_approx_eq;
 extern crate num;
 
 // NOTE(elsuizo:2019-04-06): todavia no lo necesito
 // use ndarray_linalg::*;
 pub mod transformations;
-pub mod utils;
-pub mod error;
+// pub mod utils;
+// pub mod error;
 
 //-------------------------------------------------------------------------
 //                        tests
@@ -19,9 +20,9 @@ pub mod error;
 #[cfg(test)]
 mod tests {
     use assert_approx_eq::assert_approx_eq;
-    use crate::transformations::{rotx, roty};
-    use crate::utils::cross;
-    use ndarray::arr1;
+    use crate::transformations::{rotx};
+    // use crate::utils::cross;
+    // use ndarray::arr1;
     // use ndarray_linalg::close_l1;
 
     #[test]
@@ -37,33 +38,33 @@ mod tests {
             }
         }
     }
-
-    #[test]
-    fn test_roty() {
-        let a = roty(90.0);
-        let b = roty(90.0 + 360.0);
-        for row in 0..3 {
-            for column in 0..3 {
-                assert_approx_eq!(a[[row, column]] as f64, b[[row, column]] as f64, 1.0e-6);
-            }
-        }
-    }
-
-    #[test]
-    fn test_cross() {
-        // testing X x Y = Z
-        let u = arr1(&[1.0, 0.0, 0.0]);
-        let v = arr1(&[0.0, 1.0, 0.0]);
-
-        match cross(&u, &v) {
-            Ok(w) => {
-                let z = arr1(&[0.0, 0.0, 1.0]);
-                for num in 0..3 {
-                    assert_approx_eq!(w[num] as f64, z[num] as f64);
-                }
-            }
-            Err(e) => println!("error: {}", e),
-        }
-
-    }
+    //
+    // #[test]
+    // fn test_roty() {
+    //     let a = roty(90.0);
+    //     let b = roty(90.0 + 360.0);
+    //     for row in 0..3 {
+    //         for column in 0..3 {
+    //             assert_approx_eq!(a[[row, column]] as f64, b[[row, column]] as f64, 1.0e-6);
+    //         }
+    //     }
+    // }
+    //
+    // #[test]
+    // fn test_cross() {
+    //     // testing X x Y = Z
+    //     let u = arr1(&[1.0, 0.0, 0.0]);
+    //     let v = arr1(&[0.0, 1.0, 0.0]);
+    //
+    //     match cross(&u, &v) {
+    //         Ok(w) => {
+    //             let z = arr1(&[0.0, 0.0, 1.0]);
+    //             for num in 0..3 {
+    //                 assert_approx_eq!(w[num] as f64, z[num] as f64);
+    //             }
+    //         }
+    //         Err(e) => println!("error: {}", e),
+    //     }
+    //
+    // }
 }
--- src/transformations.rs
+++ src/transformations.rs
@@ -2,7 +2,8 @@
 //-------------------------------------------------------------------------
 //                        imports
 //-------------------------------------------------------------------------
-use ndarray::prelude::*;
+// use ndarray::prelude::*;
+use na::{Matrix3, RowVector3};
 use num::Float;
 //-------------------------------------------------------------------------
 //                        code
@@ -14,102 +15,105 @@ use num::Float;
 /// description
 ///
 /// * `angle` - angle of rotation in degrees
-pub fn rotx<F: Float>(angle: F) -> Array2<F> {
+pub fn rotx<F: Float>(angle: F) -> Matrix3<F> {
     let one = F::one();
     let zero = F::zero();
     let c = angle.to_radians().cos();
     let s = angle.to_radians().sin();
-    arr2(&[[one, zero, zero],
-         [ zero,   c,  -s],
-         [zero,   s,   c]])
-}
 
-
-/// Brief.
-///
-/// Compute the rotation around the `y` axis(in cartesian coordinates)
-///
-/// Description
-///
-/// * `angle` - Angle of rotation in degrees
-pub fn roty<F: Float>(angle: F) -> Array2<F> {
-    let one = F::one();
-    let zero = F::zero();
-    let c = angle.to_radians().cos();
-    let s = angle.to_radians().sin();
-    arr2(&[[c,   zero,   s],
-           [zero, one, zero],
-           [-s,  zero,   c]])
-}
-
-/// Brief.
-///
-/// Compute the rotation around the `z` axis(in cartesian coordinates)
-///
-/// Description
-///
-/// * `angle` - Angle of rotation in degrees
-pub fn rotz<F: Float>(angle: F) -> Array2<F> {
-    let one = F::one();
-    let zero = F::zero();
-    let c = angle.to_radians().cos();
-    let s = angle.to_radians().sin();
-    arr2(&[[c,   -s,   zero],
-           [s,    c,   zero],
-           [zero,  zero, one]])
-}
-
-/// Brief.
-///
-/// Convert a Rotation Matrix to a
-///
-/// Function arguments:
-/// `r`: Array2<Float>
-///
-fn rot2trans<F: Float>(r: &Array2<F>) -> Array2<F> {
-    let mut R = Array2::<F>::zeros((4,4));
-    for row in 0..3 {
-        for column in 0..3 {
-            R[[row, column]] = r[[row, column]];
-        }
-    }
-    R[[3, 3]] = F::one();
+    let R = Matrix3::from_rows(&[
+         RowVector3::new(one, zero, zero),
+         RowVector3::new(zero,   c,  -s),
+         RowVector3::new(zero,   s,   c),]);
     return R;
 }
 
-/// Brief.
-///
-/// Compute the rotation around the `x` axis(in cartesian coordinates)
-///
-/// Function arguments:
-///  `angle`: Float
-///
-fn trotx<F: Float>(angle: F) -> Array2<F> {
-    rot2trans(&rotx(angle.to_radians()))
-}
-
-fn troty<F: Float>(angle: F) -> Array2<F> {
-    rot2trans(&roty(angle.to_radians()))
-}
-
-fn trotz<F: Float>(angle: F) -> Array2<F> {
-    rot2trans(&rotz(angle.to_radians()))
-}
-
-fn euler2rot<F: Float>(angle_phi: F, angle_theta: F, angle_psi: F) -> Array2<F> {
-    rotz(angle_phi) * roty(angle_theta) * rotz(angle_psi)
-}
-
-
-/// Brief.
-///
-/// Compute the euler angles from a Rotation matrix(ZYZ convention)
-///
-/// Function arguments:
-/// `R`: Rotation matrix
-///
-/// Output:
-/// A tuple with the angles: phi, theta, psi
-pub fn rot2euler<F: Float>(R: Array2<F>) -> (F, F, F) {
-    if
-}
+//
+// /// Brief.
+// ///
+// /// Compute the rotation around the `y` axis(in cartesian coordinates)
+// ///
+// /// Description
+// ///
+// /// * `angle` - Angle of rotation in degrees
+// pub fn roty<F: Float>(angle: F) -> Array2<F> {
+//     let one = F::one();
+//     let zero = F::zero();
+//     let c = angle.to_radians().cos();
+//     let s = angle.to_radians().sin();
+//     arr2(&[[c,   zero,   s],
+//            [zero, one, zero],
+//            [-s,  zero,   c]])
+// }
+//
+// /// Brief.
+// ///
+// /// Compute the rotation around the `z` axis(in cartesian coordinates)
+// ///
+// /// Description
+// ///
+// /// * `angle` - Angle of rotation in degrees
+// pub fn rotz<F: Float>(angle: F) -> Array2<F> {
+//     let one = F::one();
+//     let zero = F::zero();
+//     let c = angle.to_radians().cos();
+//     let s = angle.to_radians().sin();
+//     arr2(&[[c,   -s,   zero],
+//            [s,    c,   zero],
+//            [zero,  zero, one]])
+// }
+//
+// /// Brief.
+// ///
+// /// Convert a Rotation Matrix to a
+// ///
+// /// Function arguments:
+// /// `r`: Array2<Float>
+// ///
+// fn rot2trans<F: Float>(r: &Array2<F>) -> Array2<F> {
+//     let mut R = Array2::<F>::zeros((4,4));
+//     for row in 0..3 {
+//         for column in 0..3 {
+//             R[[row, column]] = r[[row, column]];
+//         }
+//     }
+//     R[[3, 3]] = F::one();
+//     return R;
+// }
+//
+// /// Brief.
+// ///
+// /// Compute the rotation around the `x` axis(in cartesian coordinates)
+// ///
+// /// Function arguments:
+// ///  `angle`: Float
+// ///
+// fn trotx<F: Float>(angle: F) -> Array2<F> {
+//     rot2trans(&rotx(angle.to_radians()))
+// }
+//
+// fn troty<F: Float>(angle: F) -> Array2<F> {
+//     rot2trans(&roty(angle.to_radians()))
+// }
+//
+// fn trotz<F: Float>(angle: F) -> Array2<F> {
+//     rot2trans(&rotz(angle.to_radians()))
+// }
+//
+// fn euler2rot<F: Float>(angle_phi: F, angle_theta: F, angle_psi: F) -> Array2<F> {
+//     rotz(angle_phi) * roty(angle_theta) * rotz(angle_psi)
+// }
+//
+//
+// /// Brief.
+// ///
+// /// Compute the euler angles from a Rotation matrix(ZYZ convention)
+// ///
+// /// Function arguments:
+// /// `R`: Rotation matrix
+// ///
+// /// Output:
+// /// A tuple with the angles: phi, theta, psi
+// pub fn rot2euler<F: Float>(R: Array2<F>) -> (F, F, F) {
+//     if
+// }
